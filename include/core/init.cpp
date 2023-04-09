c64d13867dc64db62ced8bfabda239eb83f2e524
Vladyslav Rehan <rehanvladyslav@gmail.com>
2023-04-09 22:59:51 +0300

    Rearrange calls to log functions, add curs_set()

--- include/core/init.cpp
+++ include/core/init.cpp
@@ -6,17 +6,25 @@ Parameters status;
 int init(void) {
 	atexit(deinit);
 
-	curs_set(0);
-	
-	if ((window.stdscr = initscr()) == NULL)
-		fatal_error("Could not initialize standart window");
+	window.stdscr = initscr();
 
 	if ((window.log = newwin(0,0,0,0)) == NULL)
 		fatal_error("Could not initialize log window");
+	else {	
+		log("Initialized standart window succsessfully");
+		log("Initialized log window succsessfully");
+	}
 
 	if ((window.help = newwin(0,0,0,0)) == NULL)
 		fatal_error("Could not initialize help window");
+	else
+		log("Initialized help window succsessfully");
 	
+	if (curs_set(0) == ERR)
+		log("This terminal does not support cursor visibilyty settings");
+	else
+		log("This terminal does support cursor visibilyty settings");
+		
 	return EXIT_SUCCESS;
 }
 
