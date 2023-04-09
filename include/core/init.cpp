7f8aaac2eba0536409b7795bf15fc6388ee497b5
Vladyslav Rehan <rehanvladyslav@gmail.com>
2023-04-09 21:59:30 +0300

    Create and fill init.cpp

---
+++ include/core/init.cpp
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+Windows window;
+Parameters status; 
+
+int init(void) {
+	atexit(deinit);
+	
+	if ((window.stdscr = initscr()) == NULL)
+		fatal_error("Could not initialize standart window");
+
+	if ((window.log = newwin(0,0,0,0)) == NULL)
+		fatal_error("Could not initialize log window");
+
+	if ((window.help = newwin(0,0,0,0)) == NULL)
+		fatal_error("Could not initialize help window");
+	
+	return EXIT_SUCCESS;
+}
+
+void deinit(void) {
+	endwin();
+}
+
