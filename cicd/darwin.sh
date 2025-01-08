828fda6da5afa7518474f749c7cd4a166cbecfcf
Vladyslav Rehan <rehanvladyslav@gmail.com>
2025-01-08 19:58:49 +0200

    Improve CI for darwin

--- cicd/darwin.sh
+++ cicd/darwin.sh
@@ -5,20 +5,12 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 
-case "$1" in
-	'docs'|'pack')
-		brewformulas=" ncurses"
-		brewcasks=""
-		;;
-esac
-
 # installing homebrew 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # installing dependencies
-brew install --formula cmake git $brewformulas
-brew install --cask $brewcasks
+brew install --formula cmake git ncurses
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 export REPO="$SCRIPT_DIR/.."
