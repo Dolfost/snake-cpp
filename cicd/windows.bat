14fefc20badec2539afe30c117acdac7e5cde4fe
Vladyslav Rehan <rehanvladyslav@gmail.com>
2025-01-08 21:00:54 +0200

    Try to fix CI for windows

--- cicd/windows.bat
+++ cicd/windows.bat
@@ -6,7 +6,7 @@ REM install chocolatey
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 REM figure out dependencies
-SET "chocodeps=msys2"
+SET "chocodeps=mingw"
 REM install dependencies
 IF "%chocodeps%" == "" (
 	ECHO "No packages specified to install."
@@ -14,10 +14,8 @@ IF "%chocodeps%" == "" (
 	choco install %chocodeps%
 )
 
-C:\\msys64\\usr\\bin\\env MSYSTEM=MSYS /usr/bin/bash -l -c "pacman -S mingw-w64-x86_64-ncurses mingw-w64-ucrt-x86_64-gcc cmake git"
-
 SET "SCRIPT_DIR=%~dp0"
 SET "REPO=%SCRIPT_DIR%.."
 SET "CONFIGURATION_OPTIONS=-G MinGW Makefiles"
 
-C:\\msys64\\usr\\bin\\env MSYSTEM=MSYS /usr/bin/bash -l -c "cmake -P %SCRIPT_DIR%cmake\%1.cmake"
+cmake -P "%SCRIPT_DIR%cmake\%1.cmake"
