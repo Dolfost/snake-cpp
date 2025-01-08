9a3a0a3d279f14ba478e6b47f5bad69f196eb99f
Vladyslav Rehan <rehanvladyslav@gmail.com>
2025-01-08 20:18:06 +0200

    Try to fix CI for windows

--- cicd/windows.bat
+++ cicd/windows.bat
@@ -6,7 +6,7 @@ REM install chocolatey
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 REM figure out dependencies
-SET "chocodeps=mingw"
+SET "chocodeps=mingw msys2"
 REM install dependencies
 IF "%chocodeps%" == "" (
 	ECHO "No packages specified to install."
